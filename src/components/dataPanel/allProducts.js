d24bd6cc763244d7aab51581ec265ba0ff931bac
lucasangiorama <angioramaluca@gmail.com>
2021-10-27 19:21:30 -0300

    logica de dashboard

--- src/components/dataPanel/allProducts.js
+++ src/components/dataPanel/allProducts.js
@@ -16,7 +16,7 @@ function AllProducts(props){
 	}, [])
 
 
-    const [imagen, setImagen] = useState([]);
+    /*const [imagen, setImagen] = useState([]);
 
     useEffect(() => {
 		
@@ -25,20 +25,31 @@ function AllProducts(props){
 			.then(data => {
                 let producto = data.products
                 
+                let img = []
                 
                 for (let i = 0; i < producto.length; i++) {
-                    let imagenes = producto[i].img.split(";")
-                    let img = imagenes[0]
-                    console.log("imagenes    " +  img) // devuelve el logo de cada producto
+                    let imagenes = producto[i].img.split(";") 
+                    img += imagenes[0]+";"
                 }
-                //me falta saber como pasar esa info a imagen de fetch
-                setImagen( imagen)
+                let imgLogo = img.split(";")
+                setImagen( imgLogo)
 				
             })
 			.catch(error => console.error(error));
-	}, [])
+	}, [])*/
 
-
+    
+    let productos = [];
+        product.map((producto, i) =>{
+            let img = producto.img.split(";")[0]
+            productos.push( 
+                <div key={i}>  
+                    <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" src={"  http://localhost:3001/img/" + img}  alt="image dummy" key={i}/>
+                    <h4>{producto.titulo}</h4>
+                    <p>${producto.precio}</p>
+                </div>)
+        })
+        //console.log("mapaproducto   " + product)
 
 
     return(
@@ -47,18 +58,11 @@ function AllProducts(props){
                 <div className="card-header py-3">
                     <h6 className="m-0 font-weight-bold text-primary">All product</h6>
                 </div>
-                
-                { product.map((producto, i) =>{
-                    return(
-                    <div className="card-body" key={i}>
-                    <div className="text-center">
-                        <img className="img-fluid px-3 px-sm-4 mt-3 mb-4"  alt="image dummy"/>
+                <div className="card-body" >
+                <div className="text-center">
+                        {productos}
                     </div>
-                    <h4>{producto.titulo}</h4>
-                    <p>${producto.precio}</p>
-                    </div>
-                )})  
-                }
+                </div>
             </div>
         </div>
     ); 
